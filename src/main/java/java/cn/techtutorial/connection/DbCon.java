da637ead5c07757fe1a968acbb918451295d1b71
azianaaziz <azianaaziz46@gmail.com>
2025-06-18 17:28:08 +0800

    Update DBCon

--- src/main/java/java/cn/techtutorial/connection/DbCon.java
+++ src/main/java/java/cn/techtutorial/connection/DbCon.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DbCon {
-    private static final String DB_URL = "jdbc:mysql://switchback.proxy.rlwy.net:49496/railway";
+    private static final String DB_URL = "jdbc:mysql://switchback.proxy.rlwy.net:49496/petshop";
     private static final String DB_USER = "root"; 
     private static final String DB_PASS = "CaxLOWsGEHcaUIMpeUqtCmLucuHVTNGS"; 
 
